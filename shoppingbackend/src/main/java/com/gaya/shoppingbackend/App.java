d5b3a0f3f97cb508bd1402820aedfc73f687f21d
manish760 <kr.manish3030@gmail.com>
2018-11-30 20:34:25 +0530

    My project Setup done successfully

---
+++ shoppingbackend/src/main/java/com/gaya/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.gaya.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
